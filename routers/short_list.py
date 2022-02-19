c24df60053a636c87a64222c048299a6f3e7ffc4
Plato Puthur <platoputhur@gmail.com>
2022-02-20 03:45:04 +0530

    Fixed a bug which caused to show incorrect stocks as shortliste

--- routers/short_list.py
+++ routers/short_list.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from managers.bal.short_list_manager import ShortListManager
 from managers.dal.sqlalchemy import get_db
-from models.schemas.responses import ShortListResponse
+from models.schemas.responses import ShortListResponse, ShortListedStocksResponse
 
 router = APIRouter(
     prefix="/shortlist",
@@ -16,6 +16,17 @@ def create_short_list(db: Session = Depends(get_db)):
     short_list_manager = ShortListManager(db)
     try:
         response_data = short_list_manager.create_short_list()
+        return response_data
+    except (ConnectionError, AttributeError) as ce:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Data couldn't be fetched at this time. Error: {ce}")
+
+
+@router.get("/get/latest", response_model=ShortListedStocksResponse)
+def create_short_list(db: Session = Depends(get_db)):
+    short_list_manager = ShortListManager(db)
+    try:
+        response_data = short_list_manager.fetch_latest_short_list()
         return response_data
     except (ConnectionError, AttributeError) as ce:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
